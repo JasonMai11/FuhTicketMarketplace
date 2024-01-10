e45bd9c6bd6bb8db6a46dee192b06130c65810ab
Jason Mai <maijason112102@gmail.com>
2024-01-10 15:41:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,68 +4,14 @@ import { Routes, Route} from 'react-router-dom'
 import Layout from './components/Layout'
 import Sell from './components/Sell'
 import Home from './components/Home'
-import { useEffect, useState } from "react";
-import { ethers } from 'ethers';
-
-// ABIs
-import TokenMasterABI from './abis/TokenMaster.json';
-
-// Config
-import config from './config.json';
 
 
 function App() {
 
-    const [provider, setProvider] = useState(null);
-    const [account, setAccount] = useState(null);
-    const [tokenMaster, setTokenMaster] = useState(null);
-
-    const [events, setEvents] = useState([]);
-
-    const [event, setEvent] = useState({})
-    const [toggle, setToggle] = useState(false);
-
-    const loadBlockChainData = async () => {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        setProvider(provider);
-
-        // get the current network
-        const network = await provider.getNetwork();
-
-        // get the contract address
-        const address = config[network.chainId].TokenMaster.address
-
-        // deploy the contract
-        const tokenMaster = new ethers.Contract(address, TokenMasterABI, provider);
-        setTokenMaster(tokenMaster);
-        
-
-        const totalEvents = await tokenMaster.totalEvents();
-        const events = []
-        for (var i = 1; i <= totalEvents; i++) {
-            const event = await tokenMaster.getEvent(i);
-            events.push(event);
-        }
-        setEvents(events);
-        
-
-        // Refresh Account (when the user changes their wallet, the account will update)
-        window.ethereum.on("accountsChanged", async function() {
-            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            const account = ethers.utils.getAddress(accounts[0]);
-            setAccount(account);
-            console.log(accounts[0], " is connected to the contract");
-        })
-        console.log('Connected to MetaMask');
-    }
-
-    useEffect(() => {
-        loadBlockChainData();
-    }, [])
     return (
         <>        
         <Routes>
-            <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />}>
                 <Route path = "/" index element = {<Home />} />
                 <Route path = "sell" element = {<Sell />} />
             </Route>
